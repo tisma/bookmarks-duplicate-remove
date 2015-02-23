41cbb8013dcbce3cb6c2eb866915ebccf4a83d42
Miroslav Tisma <tisma@linux.com>
2015-02-23 16:57:02 +0100

    Code refactoring and finished parsing file with changing href in links

--- Bookmarks/src/com/tisma/bookmarks/Bookmarks.java
+++ Bookmarks/src/com/tisma/bookmarks/Bookmarks.java
@@ -2,110 +2,112 @@ package com.tisma.bookmarks;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
 public class Bookmarks {
 
-private static void parse(File f, Set<String> set, List<String> list) throws IOException {
-	int i = 0;
-	Document d = Jsoup.parse(f, "UTF-8");
-
-	Elements link = d.select("a[href]");
-	for (Element l : link) {
-		URL url = new URL(l.attr("abs:href"));
-		StringBuilder sb = new StringBuilder(64);
-		if (url.toString().contains("javacodegeeks")) {
-			sb.append(url.getProtocol()).append("://").append(url.getHost()).append(url.getPath());
-			i++;
-			System.out.println(i + ": " + sb.toString());
-			set.add(sb.toString());
-			list.add(sb.toString());
-//			sb.setLength(0);
-//			System.out.println(set);
-		}
-	}
-}
-
-private static String parse(String line) {
+	private File inputBookmarksFile = null;
+	private File outputBookmarksFile = null;
+	private BufferedReader br = null;
+	private PrintWriter pw = null;
+	private Set<String> set = null;
 	
-	Document d = Jsoup.parse(line, "UTF-8");
-	Elements link = d.select("a[href]");
-	URL url = null;
-	StringBuilder sb = new StringBuilder(128);
-	
-	for (Element l : link) {
-		try {
-			url = new URL(l.attr("abs:href"));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		sb.append(url.getProtocol()).append("://").append(url.getHost()).append(url.getPath());
+	public Bookmarks() throws FileNotFoundException {
+		inputBookmarksFile = new File("bookmarks_10_18_14.html");
+		outputBookmarksFile = new File("bookmarks_out.html");
+		br = new BufferedReader(new FileReader(inputBookmarksFile));
+		pw = new PrintWriter(outputBookmarksFile);
+		set = new LinkedHashSet<String>();
 	}
 	
-	return sb.toString();
-}
-
-private static void parseCurrentLine(String currentLine, Set<String> set) {
-	if (currentLine.contains("javacodegeeks")) {
-		String parsedLine = parse(currentLine);
-		set.add(parsedLine);
-	} else {
-		set.add(currentLine);
-	}
-}
-
-private static void writeSetFile(Set<String> set, FileWriter fw) {
-	
-}
-
-	public static void main(String[] args) {
-
-		File f = new File("bookmarks_10_18_14.html");
-		File out = new File("bookmars_out.html");
-		Set<String> set = new LinkedHashSet<String>();
-		List<String> list = new ArrayList<String>();
-		BufferedReader br = null;
-		FileWriter fw = null;
+	public Bookmarks(String inputFileName, String outputFileName) throws FileNotFoundException {
+		assert(inputFileName != null);
+		assert(outputFileName != null);
 		
-		int i = 0;
+		inputBookmarksFile = new File(inputFileName);
+		outputBookmarksFile = new File(outputFileName);
+		br = new BufferedReader(new FileReader(inputBookmarksFile));
+		pw = new PrintWriter(outputBookmarksFile);
+		set = new LinkedHashSet<String>();
+	}
+	
+	public void parseBookmarksFile() {
+		String currentLine = null;
 		try {
-			br = new BufferedReader(new FileReader(f));
-			fw = new FileWriter(out);
-			String currentLine = br.readLine();
+			currentLine = br.readLine();
 			while (currentLine != null) {
-//				System.out.println(currentLine);
-				++i;
-				parseCurrentLine(currentLine, set);
+				if (currentLine.contains("javacodegeeks")) {
+					String parsedLine = parseCurrentLine(currentLine);
+					set.add(parsedLine);
+				} else {
+					set.add(currentLine);
+				}
 				currentLine = br.readLine();
 			}
-			writeSetFile(set, fw);
-			fw.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				fw.close();
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					br = null;
+				}
 			}
 		}
+	}
+	
+	public String parseCurrentLine(String currentLine) {
+		Document d = Jsoup.parse(currentLine, "UTF-8", Parser.xmlParser());
+		Elements link = d.select("a[href]");
+		URL url = null;
+		StringBuilder sb = new StringBuilder(128);
 
-		System.out.println("set: " + set.size() + ", list: " + list.size() + " i: " + i);
+		for (Element l : link) {
+			try {
+				url = new URL(l.attr("abs:href"));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			sb.append(url.getProtocol()).append("://").append(url.getHost())
+					.append(url.getPath());
+		}
 
+		d.attr("href", sb.toString());
+		return d.toString();
 	}
 
+	public void writeToOutputBookmarksFile() {
+		for (String s : set) {
+			pw.write(s + "\n");
+		}
+		pw.flush();
+		pw.close();
+		pw = null;
+	}
+
+	public static void main(String[] args) {
+		try {
+			Bookmarks bookmarks = new Bookmarks();
+			bookmarks.parseBookmarksFile();
+			bookmarks.writeToOutputBookmarksFile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
